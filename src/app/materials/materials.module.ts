99466ddf63f397ab94ddeda6a56282b70d5b9ae3
parimalmahindrakar <parimalm4653@gmail.com>
2021-03-13 14:21:15 +0530

    added the typegraphy class

--- src/app/materials/materials.module.ts
+++ src/app/materials/materials.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 
 const MaterialComponents = [
-  MatButtonModule
+  MatButtonModule,
+
 ]
 
 @NgModule({
@@ -15,3 +16,11 @@ const MaterialComponents = [
   ]
 })
 export class MaterialsModule { }
+
+
+/*
+  Typography basically deals with the styles and apperance of the 
+  Text on your website !
+  Angular provides the levels where each level will have particular fontsize,
+  font-weight, etc.
+*/
