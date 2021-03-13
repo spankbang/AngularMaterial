56cff8d18c8d0ed30d13758c2dd32a35b61dd962
parimalmahindrakar <parimalm4653@gmail.com>
2021-03-13 16:01:18 +0530

    added code

--- src/app/materials/materials.module.ts
+++ src/app/materials/materials.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
-
-import { MatButtonModule } from '@angular/material/button';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
 
 const MaterialComponents = [
-  MatButtonModule,
-  MatButtonToggleModule
+  MatIconModule
 ]
 
 @NgModule({
