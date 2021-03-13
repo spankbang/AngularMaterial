76fed067f2a56386fc6f69e11614887603efa7fe
parimalmahindrakar <parimalm4653@gmail.com>
2021-03-13 13:58:30 +0530

    added very initiate code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+// angular material imports
+import { MatButtonModule } from '@angular/material/button';
+// ----------------------------
 
 @NgModule({
   declarations: [
@@ -10,7 +16,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    // angular material imports
+
+    MatButtonModule,
+    
+
+    // ------------------------
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
