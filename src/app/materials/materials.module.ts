954ebc79e905294633075e015702a6236234a86f
parimalmahindrakar <parimalm4653@gmail.com>
2021-03-13 17:44:23 +0530

    added code

--- src/app/materials/materials.module.ts
+++ src/app/materials/materials.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav'
-import  { MatButtonModule } from '@angular/material/button'
+import { MatButtonModule } from '@angular/material/button'
+import { MatMenuModule } from '@angular/material/menu'
+
 
 const MaterialComponents = [
   MatSidenavModule,
-  MatButtonModule
+  MatButtonModule,
+  MatMenuModule
 ]
 
 @NgModule({
