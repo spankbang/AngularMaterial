da19e9ef29630a7aa53c8ff2739ca80165627c6e
parimalmahindrakar <parimalm4653@gmail.com>
2021-03-13 23:31:28 +0530

    added the code

--- src/app/materials/materials.module.ts
+++ src/app/materials/materials.module.ts
@@ -1,22 +1,14 @@
 import { NgModule } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav'
 import { MatButtonModule } from '@angular/material/button'
-import { MatListModule } from '@angular/material/list'
-import { MatIconModule } from '@angular/material/icon'
-import { MatDividerModule } from '@angular/material/divider'
-import { MatGridListModule } from '@angular/material/grid-list'
-
-
-
-
+import { MatExpansionModule } from '@angular/material/expansion'
 
 
 const MaterialComponents = [
   MatSidenavModule,
   MatButtonModule,
-  MatIconModule,
-  MatListModule,
-  MatGridListModule
+  MatExpansionModule
+  
 ]
 
 @NgModule({
