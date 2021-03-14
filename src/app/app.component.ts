1dd7266270596719c4006656856445c4a5d10a8b
parimalmahindrakar <parimalm4653@gmail.com>
2021-03-14 08:07:53 +0530

    autocompltes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  selectedValue:String
-
+  options: string[] = ["angular", "react", "vue"]
+  matAutoComplete:string
 
 }
--- src/app/materials/materials.module.ts
+++ src/app/materials/materials.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button'
-import { MatSelectModule } from '@angular/material/select'
+import { MatAutocompleteModule } from '@angular/material/autocomplete'
+import { MatInputModule } from '@angular/material/input'
+
+
 
 
 
 const MaterialComponents = [
   
   MatButtonModule,
-  MatSelectModule
-  
+  MatAutocompleteModule,
+  MatInputModule
 ]
 
 @NgModule({
