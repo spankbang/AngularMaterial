5850aa748cee8240f5a85ed0e5a0e4c37f51880f
parimalmahindrakar <parimalm4653@gmail.com>
2021-03-14 07:35:59 +0530

    inputs

--- src/app/materials/materials.module.ts
+++ src/app/materials/materials.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button'
-import { MatStepperModule } from '@angular/material/stepper'
+import { MatInputModule } from '@angular/material/input'
 
 
 
 const MaterialComponents = [
   
   MatButtonModule,
-  MatStepperModule
+  MatInputModule
   
 ]
 
